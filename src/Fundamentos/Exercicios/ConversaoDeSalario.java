5b069ac802409f82526ef6de77a2a5062f1a6b5a
SousaaArthur <arthursousaa2004@gmail.com>
2024-09-06 10:37:44 -0300

    Atualização repositorio Java_couse

---
+++ src/Fundamentos/Exercicios/ConversaoDeSalario.java
@@ -0,0 +1,25 @@
+package Fundamentos.Exercicios;
+
+import java.util.Scanner;
+
+public class ConversaoDeSalario {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Insira o primeiro salario: ");
+    String wageString1 = sc.nextLine().replace(",", ".");
+    double wage1 = Double.parseDouble(wageString1);
+    System.out.print("Insira o segundo salario: ");
+    String wageString2 = sc.nextLine().replace(",", ".");
+    double wage2 = Double.parseDouble(wageString2);
+    System.out.print("Insira o terceiro salario: ");
+    String wageString3 = sc.nextLine().replace(",", ".");
+    double wage3 = Double.parseDouble(wageString3);
+
+    double sum = (wage1 + wage2 + wage3) / 3;
+
+    System.out.printf("A média dos três salário é de: %.2f", sum);
+
+    sc.close();
+  }
+}
--- src/Fundamentos/Exercicios/Temperatura.java
+++ src/Fundamentos/Exercicios/Temperatura.java
@@ -1,4 +1,5 @@
-package Fundamentos;
+package Fundamentos.Exercicios;
+
 import java.util.Scanner;
 
 public class Temperatura {
