9d70a7d5e5cd72fe50690162dc367ffe6b77d079
SousaaArthur <arthursousaa2004@gmail.com>
2024-09-10 11:46:20 -0300

    Atualização Aulas curso Java

---
+++ src/IgualdadeComStrings.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class IgualdadeComStrings {
+  public static void main(String[] args) {
+    
+    System.out.println("2" == "2");
+
+    String s = (new String("2"));
+    System.out.println("2" == s);
+    System.out.println("2".equals(s));
+
+    Scanner sc = new Scanner(System.in);
+
+    String s2 = sc.next();
+
+    System.out.println("2".equals(s2.trim()));
+
+    sc.close();
+  }
+}
