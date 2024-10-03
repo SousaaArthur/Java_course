5d64d6bb3ba09a47e43880c42cf71e00695aced1
SousaaArthur <arthursousaa2004@gmail.com>
2024-10-03 11:48:45 -0300

    Ataulização repositorio curso classes e metodos

--- src/ClassesEMetodos/Data.java
+++ src/ClassesEMetodos/Data.java
@@ -6,23 +6,25 @@ public class Data {
   int ano;
 
   Data(){
-    dia = 1;
-    mes = 1;
-    ano = 1970;
+    // dia = 1;
+    // mes = 1;
+    // ano = 1970;
+    this(1, 1, 1970);
   }
 
-  Data(int diaInicial, int mesInicial, int anoInicial){
-    dia = diaInicial;
-    mes = mesInicial;
-    ano = anoInicial;
+  Data(int dia, int mes, int ano){
+    this.dia = dia;
+    this.mes = mes;
+    this.ano = ano;
   }
 
   String dateFormatted(){
-    return String.format("%02d/%02d/%02d", dia, mes,ano);
+    final String formato = "%02d/%02d/%02d";
+    return String.format(formato, dia, mes,ano);
   }
 
   void printDateFormatted(){
-    System.out.println(dateFormatted());
+    System.out.println(this.dateFormatted());
   }
 
 }
